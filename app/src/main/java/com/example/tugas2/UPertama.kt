c4c695ec5e0c99a3452beeb7cb919724e7bb3ffb
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-10-23 09:30:51 +0700

    Menambahkan jarak bawah 50dp pada komponen di tampilan UI

--- app/src/main/java/com/example/tugas2/UPertama.kt
+++ app/src/main/java/com/example/tugas2/UPertama.kt
@@ -78,6 +78,8 @@ fun ActivitasPertama(modifier : Modifier){
             text = stringResource(id = R.string.copy),
             modifier = Modifier
                 .align(Alignment.BottomCenter)
+                .padding(bottom = 50.dp)
+
 
 
 
