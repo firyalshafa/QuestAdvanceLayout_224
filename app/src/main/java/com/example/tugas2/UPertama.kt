c8af4932e10555b9e7805008ccf2a3d5c061607e
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-10-23 09:07:00 +0700

    Menambahkan tampilan kartu berwarna abu tua dengan padding dan lebar penuh

--- app/src/main/java/com/example/tugas2/UPertama.kt
+++ app/src/main/java/com/example/tugas2/UPertama.kt
@@ -33,6 +33,13 @@ fun ActivitasPertama(modifier : Modifier){
             fontSize = 35.sp, fontWeight = FontWeight.Bold)
         Text(text = stringResource(id = R.string.univ),
             fontSize = 22.sp)
+        Spacer(modifier= Modifier.height(height = 25.dp))
+        Card (modifier = Modifier
+            .fillMaxWidth(fraction = 1f)
+            .padding(all = 12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
+            )){
 
         }
 
