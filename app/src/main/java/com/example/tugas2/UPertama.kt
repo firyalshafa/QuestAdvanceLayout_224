d28dd52763b7ab1c7d2b86c0060aa29ceccce9c2
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-10-23 09:05:46 +0700

    menambahkan teks nama prodi dan universitas di layar utama

--- app/src/main/java/com/example/tugas2/UPertama.kt
+++ app/src/main/java/com/example/tugas2/UPertama.kt
@@ -1,9 +1,41 @@
 package com.example.tugas2
 
+import android.graphics.fonts.FontFamily
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+//import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import java.lang.reflect.Modifier
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 
 @Composable
-fun ActivitasPertama(modifier: Modifier){
+fun ActivitasPertama(modifier : Modifier){
+    Column(modifier = modifier.padding(top=100.dp).fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally){
+        Text(text = stringResource(id = R.string.prodi),
+            fontSize = 35.sp, fontWeight = FontWeight.Bold)
+        Text(text = stringResource(id = R.string.univ),
+            fontSize = 22.sp)
+
+        }
+
+    }
 
 }
